f7444003d01598ba76998a276507ba72c7093d8f
MarkOsBab <m.g4089188@gmail.com>
2022-11-17 10:35:11 -0300

    Last animations

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+  faGlobe = faGlobe;
   constructor() { }
 
   ngOnInit(): void {
